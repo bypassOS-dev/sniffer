2727ed3407e98a99ccc4c836b4d56459492c9db0
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-09-15 16:18:24 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,79 @@
-use std::net::Ipv4Addr;
-
 use nfq::{Queue, Verdict};
 use pnet::packet::ipv4::Ipv4Packet;
+use std::net::Ipv4Addr;
+use tokio::process::Command;
 #[tokio::main]
 async fn main() {
+    let status = Command::new("iptables")
+        .arg("-A")
+        .arg("OUTPUT")
+        .arg("-p")
+        .arg("tcp")
+        .arg("--dport")
+        .arg("443")
+        .arg("-j")
+        .arg("NFQUEUE")
+        .arg("--queue-num")
+        .arg("0")
+        .status()
+        .await
+        .expect("Executing error");
+
+    println!("Status: {status}");
+    loop {
+        tokio::select! {
+            _ = like_main() => {
+
+            }
+            _ = tokio::signal::ctrl_c() => {
+                let _status = Command::new("iptables")
+                    .arg("-D")
+                    .arg("OUTPUT")
+                    .arg("-p")
+                    .arg("tcp")
+                    .arg("-d")
+                    .arg("172.217.76.93")
+                    .arg("--dport")
+                    .arg("443")
+                    .arg("-j")
+                    .arg("NFQUEUE")
+                    .arg("--queue-num")
+                    .arg("0")
+                    .status()
+                    .await
+                    .expect("Executing error");
+
+                    std::process::exit(0);
+                }
+            }
+    }
+}
+async fn create_wrapper(ip_addr_src: Ipv4Addr, ip_addr_dest: Ipv4Addr, ttl: u8) {
+    //================================================
+    let ip_addr_str = ip_addr_src.to_string();
+    let ip_addr_len = ip_addr_str.len();
+    let do_space = 15 - ip_addr_len;
+    //=================================================
+    let ip_addr_str = ip_addr_dest.to_string();
+    let ip_addr_len = ip_addr_str.len();
+    let do_space1 = 15 - ip_addr_len;
+    //=================================================
+    let ttl = ttl.to_string();
+    let ttl_len = ttl.len();
+    let do_space2 = 3 - ttl_len;
+    //=================================================
+    println!(" ==========================================");
+    println!("|| From: {}                 {:do_space$}||",ip_addr_src, do_space);
+    println!("|| To: {}                   {:do_space1$}||",ip_addr_dest, do_space1);
+    println!("|| TTL: {}                              {:do_space2$}||",ttl, do_space2);
+    println!("||");
+    println!("||");
+    println!("||");
+    println!("||");
+    println!("||");
+    println!(" ============================================")
+}
+async fn like_main() {
     let mut queue = Queue::open().unwrap();
     queue.bind(0).unwrap();
 
@@ -15,25 +85,10 @@ async fn main() {
             let src_ip = ipv4.get_source();
             let dest_ip = ipv4.get_destination();
             let ttl = ipv4.get_ttl();
+
+            create_wrapper(src_ip, dest_ip, ttl).await;
         }
         msg.set_verdict(Verdict::Accept);
         queue.verdict(msg).unwrap();
     }
-}
-async fn create_wrapper(ip_addr_src: Ipv4Addr, ip_addr_dest: Ipv4Addr, ttl: u8) {
-    //================================================
-    let ip_addr_str = ip_addr_src.to_string();
-    let ip_addr_len = ip_addr_str.len();
-    let do_space = 15 - ip_addr_len;
-    //=================================================
-    println!(" ==========================================");
-    println!("|| From: {}                 {:do_space$}||", ip_addr_src, do_space);
-    println!("|| To: {}                                  ||", ip_addr_dest);
-    println!("|| TTL: {}                                 ||", ttl);
-    println!("||");
-    println!("||");
-    println!("||");
-    println!("||");
-    println!("||");
-    println!(" ============================================")
 }
